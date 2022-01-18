247561b6de7d3e3d4f30ca1a7647960b7fc8e2a7
Mateus Ito <mateusito10@gmail.com>
2022-01-18 02:53:57 -0300

    Add MyBottomNavBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 
+import 'components/my_bottom_nav_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,7 +11,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: const Body()
+      body: const Body(),
+      bottomNavigationBar: const MyBottomNavBar(),
     );
   }
 
